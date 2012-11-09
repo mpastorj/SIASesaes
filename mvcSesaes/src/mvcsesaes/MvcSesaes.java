fe20e4e85ac07374c106842a52d1ae0a7d8a3a98
Milii2 <Milii2@Milii-PC>
2012-11-09 00:36:51 -0400

    Versión0

---
+++ mvcSesaes/src/mvcsesaes/MvcSesaes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvcsesaes;
+
+/**
+ *
+ * @author Milii2
+ */
+public class MvcSesaes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
